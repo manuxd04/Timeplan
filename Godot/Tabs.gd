06d4fdf33547e30b93461594d098ded9380a9688
Lindo <c@changry.no>
2021-10-12 20:13:37 +0200

    Basic layout

---
+++ Godot/Tabs.gd
@@ -0,0 +1,12 @@
+extends Tabs
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	for i in range(3):
+		add_tab("               ")
