6272c20373fab537cd135d65deec7ee53875c85c
Johannes <Jotua001@osloskolen.no>
2021-10-13 13:06:34 +0200

    Added textbox lines

--- Python/Plugins/png_export.py
+++ Python/Plugins/png_export.py
@@ -5,6 +5,18 @@ from PIL import ImageDraw
 import math
 
 #Generate Image
+def MakeTextBox(draw, ImagePos, FontSizeW, FontSizeH):
+    Width = 5
+    FontSizeH = FontSizeH - Width
+    FontSizeW = FontSizeW - Width
+    #Top Line
+    draw.line((ImagePos[0], ImagePos[1], ImagePos[0] + FontSizeW, ImagePos[1]), width = Width)
+    #Left Line
+    draw.line((ImagePos[0], ImagePos[1], ImagePos[0], ImagePos[1] + FontSizeH), width = Width)
+    #Bottom Line
+    draw.line((ImagePos[0], ImagePos[1] + FontSizeH, ImagePos[0] + FontSizeW, ImagePos[1] + FontSizeH), width = Width)
+    #Right Line
+    draw.line((ImagePos[0] + FontSizeW, ImagePos[1], ImagePos[0] + FontSizeW, ImagePos[1] + FontSizeH), width = Width)
 def ExportImage(CanvazSize, DisplayText, TextColor):
     #Display Values
     MaxFontSize = math.floor(CanvazSize[0] / 5)
@@ -12,15 +24,21 @@ def ExportImage(CanvazSize, DisplayText, TextColor):
     #Image file generation
     img = Image.new('RGB', (CanvazSize[0], CanvazSize[1]), color = 'red')
     draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype(r"/Users/johannes/Desktop/Python-Kalender-Prosjekt/Timeplan/Python/Plugins/CHELON.ttf", MaxFontSize)
+    #Text
+    font = ImageFont.truetype(r"/Users/johannes/Desktop/Python-Kalender-Prosjekt/Timeplan/Python/Plugins/CHELON.ttf", 50)
     FontSizeW, FontSizeH = draw.textsize(DisplayText, font=font)
-    draw.text(((CanvazSize[0]-FontSizeW)/2,0), DisplayText, font=font, fill = TextColor)
-    draw.text(((CanvazSize[0]-FontSizeW)/2,FontSizeH), DisplayText, font=font, fill = TextColor)
+    ImagePos = [290,200]
+    #ImagePos = [(CanvazSize[0]-FontSizeW)/2, 20]
+    draw.text((ImagePos[0],ImagePos[1]), DisplayText, font=font, fill = TextColor)
+    
+    #Image
+    MakeTextBox(draw, ImagePos, FontSizeW, FontSizeH)
+    #Saving
     img.show()
 
 #Display Conf
 #            W    H
-CanvazSize = [1000,920]
+CanvazSize = [690,420]
 DisplayText = "Hello World!"
 TextColor = (0,200,0)
 ExportImage(CanvazSize, DisplayText, TextColor)
