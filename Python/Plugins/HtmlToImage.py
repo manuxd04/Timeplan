6cf1ff4aa3a7734297033dae7c780e4a0c6b2c01
Johannes <Jotua001@osloskolen.no>
2021-10-20 12:51:02 +0200

    Code improvements

--- Python/Plugins/HtmlToImage.py
+++ Python/Plugins/HtmlToImage.py
@@ -1,17 +1,31 @@
+#https://pypi.org/project/html2image/
+from math import fabs, trunc
 import random
 import time
 import os
 from PIL import Image
 from html2image import Html2Image
 
-#Website path
+#Settings
+##Website path
 Url = 'https://mon.ruter.no/departures/59.918991000000005-10.753255000000001'
 Output = '/Users/johannes/Desktop/Python-Kalender-Prosjekt/Timeplan/Python/Plugins/my_screenshot_folder'
 CustomeSize = False
+LoadTime = False
 ImageSize = (20,500)
+WebsiteLoadTime = 1000
 
-hti = Html2Image()
-hti = Html2Image(output_path=Output)
+#Flags
+if LoadTime == False:
+    hti = Html2Image(
+        output_path=Output,
+        custom_flags=['--hide-scrollbars']
+    )
+else:
+    hti = Html2Image(
+        output_path=Output,
+        custom_flags=['--virtual-time-budget=' + str(WebsiteLoadTime), '--hide-scrollbars']
+    )
 
 ranstr = str(random.randrange(1,1000000000))
 filename = 'python_org' + ranstr + ".png"
@@ -24,7 +38,7 @@ dir = Output + "/" + filename
 img = Image.open(open(dir, 'rb'))
 img.show()
 print(dir)
-time.sleep(10)
+time.sleep(WebsiteLoadTime / 100)
 #Delete image
 for f in os.listdir(Output):
     os.remove(os.path.join(Output, f))
