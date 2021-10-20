a5573fb799e59cff511a35339b967d382f1c722e
Johannes <Jotua001@osloskolen.no>
2021-10-20 12:18:25 +0200

    Implemented html to png converter

--- Python/Plugins/HtmlToImage.py
+++ Python/Plugins/HtmlToImage.py
@@ -1,8 +1,30 @@
 import random
-
+import time
+import os
+from PIL import Image
 from html2image import Html2Image
 
+#Website path
+Url = 'https://mon.ruter.no/departures/59.918991000000005-10.753255000000001'
+Output = '/Users/johannes/Desktop/Python-Kalender-Prosjekt/Timeplan/Python/Plugins/my_screenshot_folder'
+CustomeSize = False
+ImageSize = (20,500)
+
 hti = Html2Image()
-hti = Html2Image(output_path='/Users/johannes/Desktop/Python-Kalender-Prosjekt/Timeplan/Python/Plugins/my_screenshot_folder')
+hti = Html2Image(output_path=Output)
+
 ranstr = str(random.randrange(1,1000000000))
-hti.screenshot(url='https://elvebakken.vgs.no/', save_as='python_org' + ranstr + ".png")
+filename = 'python_org' + ranstr + ".png"
+if CustomeSize == True:
+    hti.screenshot(url=Url, save_as=filename, size=(20, 500))
+else:
+    hti.screenshot(url=Url, save_as=filename)
+#Open image
+dir = Output + "/" + filename
+img = Image.open(open(dir, 'rb'))
+img.show()
+print(dir)
+time.sleep(10)
+#Delete image
+for f in os.listdir(Output):
+    os.remove(os.path.join(Output, f))
